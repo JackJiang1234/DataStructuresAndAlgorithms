0809dda60e379fd507f1b485908a9df5869a8b1e
Jack <jjy_8888@163.com>
2018-11-21 14:37:06 +0800

    update LRUCache impl

--- impl/src/main/java/LinkLRUCache.java
+++ impl/src/main/java/LinkLRUCache.java
@@ -1,3 +1,5 @@
+import com.sun.corba.se.impl.ior.OldJIDLObjectKeyTemplate;
+
 import java.util.ArrayList;
 
 /**
@@ -8,50 +10,98 @@ import java.util.ArrayList;
  */
 public class LinkLRUCache {
 
+    private final int capacity;
     private Node header;
     private int count;
-    private final int capacity;
 
-    public LinkLRUCache(int capacity){
+    public LinkLRUCache(int capacity) {
         this.capacity = capacity;
         this.count = 0;
-    }
-
-    public int getCapacity(){
-        return this.capacity;
-    }
-
-    public int getCount(){
-        return this.count;
-    }
-
-    public Object getData(Object key){
-        return null;
-    }
-
-    public void setData(){
-
-    }
-
-    public boolean isFull(){
-        return this.count == this.capacity;
+        this.header = new Node();
     }
 
     public static void main(String[] args) {
+        LinkLRUCache cache = new LinkLRUCache(10);
+        for (int i = 0; i < 11; i++) {
+            cache.set("name" + i, "jack" + i);
+        }
 
+        for (int i = 0; i < cache.getCount(); i++){
+            System.out.println(cache.get("name" + i));
+        }
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public Object get(Object key) {
+        KeyValePair pair = this.findKeyValuePair(key);
+        return pair == null ? null : pair.getValue();
+    }
+
+    public void set(Object key, Object value) {
+        KeyValePair pair = this.findKeyValuePair(key);
+        if (pair != null) {
+            pair.setValue(value);
+            return;
+        }
+        if (this.isFull()) {
+            Node current = this.header.getNext();
+            Node prev = this.header;
+            while (current.getNext() != null) {
+                prev = current;
+                current = current.getNext();
+            }
+            prev.setNext(null);
+            this.count--;
+        }
+
+        Node newNode = new Node();
+        newNode.setKeyValePair(new KeyValePair(key, value));
+        newNode.setNext(this.header.getNext());
+        this.header.setNext(newNode);
+        this.count++;
+    }
+
+    public boolean isFull() {
+        return this.count == this.capacity;
+    }
+
+    private KeyValePair findKeyValuePair(Object key) {
+        Node current = header.getNext();
+        Node prev = header;
+        while (current != null) {
+            KeyValePair pair = current.getKeyValePair();
+            if (pair.getKey().equals(key)) {
+                prev.setNext(current.getNext());
+                current.setNext(this.header.getNext());
+                this.header.setNext(current);
+
+                return pair;
+            }
+            prev = current;
+            current = current.getNext();
+        }
+
+        return null;
     }
 }
 
 class Node {
-    private Object data;
+    private KeyValePair keyValePair;
     private Node next;
 
-    public Object getData() {
-        return data;
+    public KeyValePair getKeyValePair() {
+        return keyValePair;
     }
 
-    public void setData(Object data) {
-        this.data = data;
+    public void setKeyValePair(KeyValePair keyValePair) {
+        this.keyValePair = keyValePair;
     }
 
     public Node getNext() {
@@ -60,5 +110,31 @@ class Node {
 
     public void setNext(Node next) {
         this.next = next;
+    }
+}
+
+class KeyValePair {
+    private Object key;
+    private Object value;
+
+    public KeyValePair(Object key, Object value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public Object getKey() {
+        return key;
+    }
+
+    public void setKey(Object key) {
+        this.key = key;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
     }
 }
