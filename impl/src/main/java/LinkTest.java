b3bd8db2ee5976fca6626099a592a53690c006ab
Jack <jjy_8888@163.com>
2018-11-30 17:11:03 +0800

    add link test

---
+++ impl/src/main/java/LinkTest.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * 测试链表特性
+ *
+ *
+ * */
+public class LinkTest {
+    public static void main(String[] args){
+
+    }
+
+    /**
+     * 链表反转
+     * */
+
+
+    /**
+     * 有序链表合并
+     * */
+
+}
