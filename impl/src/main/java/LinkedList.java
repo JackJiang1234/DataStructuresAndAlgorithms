7092d375927d68caa4749a9b9f47afcbc19db23f
Jack <jjy_8888@163.com>
2018-12-07 17:19:40 +0800

    add link test code

--- impl/src/main/java/LinkedList.java
+++ impl/src/main/java/LinkedList.java
@@ -1,20 +1,17 @@
 /**
- *
  * 测试链表特性
- *
- *
- * */
+ */
 public class LinkedList {
 
     /**
      * 单链表反转
-     * */
+     */
     public static Node reverse(Node list) {
         Node current = list;
         Node next = list.next;
         Node pre = null;
 
-        while(current != null){
+        while (current != null) {
             current.next = pre;
             pre = current;
             current = next;
@@ -29,17 +26,17 @@ public class LinkedList {
      * 思路1. 把遍历过的链表结点存储到集合或hashtable，每遍历一个结点检查在集合中是否存在，存在则存在环，否则不存在
      * 思路2. 快慢指针，试想有一个圆形跑道，甲乙两人同时在跑道的起点，如果甲的速度是乙的两倍，
      * 那么当乙跑完半圈时，甲跑完一圈回到起点，乙跑完一圈回到起点时，甲跑完两圈也回到起点，这样的话，甲乙重新在起点相遇
-     * */
+     */
     public static boolean checkCircle(Node list) {
-        if (list == null){
+        if (list == null) {
             return false;
         }
-        Node fast,slow;
+        Node fast, slow;
         fast = slow = list;
-        while(fast != null && fast.next != null){
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
-            if (fast == slow){
+            if (fast == slow) {
                 return true;
             }
         }
@@ -49,29 +46,40 @@ public class LinkedList {
 
     /**
      * 有序链表合并
-     * */
+     */
     public static Node mergeSortedLists(Node la, Node lb) {
-       return null;
+        return null;
     }
 
     /**
      * 删除倒数第K个结点
-     * */
+     */
     public static Node deleteLastKth(Node list, int k) {
         return null;
     }
 
     /**
      * 求中间结点
-     * */
+     */
     public static Node findMiddleNode(Node list) {
-        return null;
+        if (list == null) {
+            return null;
+        }
+
+        Node slow, fast;
+        slow = fast = list;
+
+        while (fast.next != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
     }
 
     /**
      * 链表是否交叉
-     * */
-    public static boolean checkHasCross(Node list1, Node list2){
+     */
+    public static boolean checkHasCross(Node list1, Node list2) {
         return true;
     }
 
